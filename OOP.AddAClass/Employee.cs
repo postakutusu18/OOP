f5b800b9a81c0f4410af1f2fe56923ab5364aeb8
postakutusu18 <postakutusu18@hotmail.com>
2022-04-28 00:20:46 +0300

    Class İşlemleri

---
+++ OOP.AddAClass/Employee.cs
@@ -0,0 +1,72 @@
+﻿namespace OOP.AddAClass
+{
+    class Employee
+    {
+       
+        private string _firstName; //Field:alan
+
+        //başka bir sınıftan değerine ulaşmak için kapsülleme işlemi  yapılır.
+        public string FirstName //Proporties 
+        {
+            set { _firstName = value; }
+            get { return _firstName; }
+        }
+
+        //Expression bodied proporty accessors. ctrl + . ile ekledik.  (encapsulate field
+        private string _lastName;
+
+
+        
+        public string LastName { get => _lastName; set => _lastName = value; }
+
+
+        //Auto implement proporties.
+        public int Age { get; set; }
+
+
+        //Metod Tanımlama İşlemi
+        //field
+        private List<Employee> _employees;
+
+
+
+        public List<Employee> Employees =>GetEmployees();
+        public List<Employee> Employees2 => _employees;
+        public void Add(Employee employee)
+        {
+            _employees.Add(employee);
+        }
+
+        //birden fazla değerin eklenmesi
+        public void AddRange(params Employee[] employees)
+        {
+            _employees.AddRange(employees);
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return _employees;
+        }
+
+        public Employee()
+        {
+            _employees = new List<Employee>();
+            //Console.WriteLine("yapılandırıcı method çalıştırıldı");
+        }
+
+        //Overloading
+        public Employee(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            _employees=new List<Employee>();
+        }
+        //Override İşlemi -geçersiz kılma
+        public override string? ToString()
+        {
+            return $"{FirstName,-20}{LastName,-15}{Age,5}";  //eksi işareti sola yaslamak
+            //return base.ToString();
+        }
+    }
+}
---
+++ OOP.AddAClass/Program.cs
@@ -0,0 +1,56 @@
+﻿using OOP.AddAClass;
+
+Console.WriteLine("Hello, World!");
+
+Employee employee = new Employee();
+employee.FirstName = "Selçuk";
+employee.LastName = "KARAAĞAÇ";
+employee.Age = 37;
+Console.WriteLine(String.Concat(employee.FirstName," ",employee.LastName," ",employee.Age));
+
+Console.WriteLine("-------");
+Employee employee2 = new Employee("Ali", "YILMAZ",32);
+Console.WriteLine(String.Concat(employee2.FirstName, " ", employee2.LastName, " ", employee2.Age));
+
+Employee employee3 = new Employee("Ahmet","KARA",21);
+Console.WriteLine(employee3);
+
+Employee employee4 = new Employee { FirstName = "Veli", LastName = "AK", Age = 18 };
+Console.WriteLine("--- Liste ----");
+var list =new List<Employee>();
+list.Add(employee);
+list.Add(employee2);
+list.Add(employee3);
+list.Add(employee4);
+foreach (var item in list)
+    Console.WriteLine(item);
+
+var list2 = new List<Employee>() { employee,employee2,employee3,employee4 };
+foreach (var item in list2)
+    Console.WriteLine(item);
+
+
+var employe5=new Employee();
+employe5.Add(new Employee { FirstName = "Harun", LastName = "ÖZ", Age = 21 });
+
+
+//çoklu kayıt ekle.
+employe5.AddRange(new Employee("A","B",1), new Employee("C","D",2));
+Console.WriteLine("--- x ----");
+Console.WriteLine("Çalışan Listesi");
+var listEmployee=employe5.GetEmployees();
+foreach (var item in listEmployee)
+    Console.WriteLine(item);
+
+Console.WriteLine("--- x ----");
+var listEmployee2 = employe5.Employees;
+foreach (var item in listEmployee2)
+    Console.WriteLine(item);
+
+Console.WriteLine("--- x ----");
+var listEmployee3 = employe5.Employees2;
+foreach (var item in listEmployee3)
+    Console.WriteLine(item);
+
+Console.ReadLine();
+
